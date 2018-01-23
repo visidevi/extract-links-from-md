2d24759b3df52835fec326a4571adba5114a7b57
visaka <visidevi@gmail.com>
2018-01-23 00:19:46 -0300

    trabajando en el DEMO

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
-# extractLinksFromMd
-
-* **Track:** _Common Core_
-* **Curso:** _JS Deep Dive: Crea tu propia librería usando JavaScript_
-* **Unidad:** _Producto final_
-
-***
-
-Implementar un módulo de Node.js que reciba un string (en formato Markdown) y
-extraiga todos los links encontrados. La implementación debe ser una función que
-recibe un string y devuelve un arreglo de objetos como se muestra en el este
-ejemplo:
-
-```js
-const extractLinksFromMd = require('extract-links-from-md');
-const str = `# Lorem ipsum
+# Lorem ipsum
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 incididunt ut [labore](https://en.wiktionary.org/wiki/labore) et
@@ -25,14 +10,4 @@ consequat.
 
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
 fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-culpa qui officia deserunt mollit anim id est laborum.`;
-
-const links = extractLinksFromMd(str);
-
-console.log(links);
-// [
-//   { href: 'https://en.wiktionary.org/wiki/labore', text: 'labore' },
-//   { href: 'https://en.wiktionary.org/wiki/dolore', text: 'dolore' },
-//   { href: 'http://foo.com', text: 'foo' },
-// ]
-```
+culpa qui officia deserunt mollit anim id est laborum.
---
+++ markdown-link-extractor.js
@@ -0,0 +1,90 @@
+let links = [];
+class Link {
+			constructor(text,href) {
+				
+				this.text = text;
+				this.href = href;
+			}
+		}  
+
+
+
+  function extractLinksFromMd() {
+
+      	let markdown = document.getElementById('txtInput').value;
+		
+     
+        document.getElementById("resultado").innerHTML= markdown; 
+        console.log(markdown);
+
+        const re = /(\[(.*?)\])|(https?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi;
+		let matches = markdown.match(re);
+		console.log(matches);
+
+
+
+        for ( i = 0; i <matches.length; i++){
+
+			let one = new Link(matches[i],matches[i+1])
+			i++
+		    links.push(one);
+
+			}
+			console.log(JSON.stringify(links));
+			document.getElementById("resultado").innerHTML= JSON.stringify(links);
+		       
+		    };
+
+
+       
+      
+    
+/*    
+
+
+
+
+const re = /(\[(.*?)\])|(https?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi;
+let matches = markdown.match(re);
+console.log(matches);
+
+let links = [];
+
+class Link {
+	constructor(text,href) {
+		
+		this.text = text;
+		this.href = href;
+	}
+}
+
+
+  function e(){
+
+   	for ( i = 0; i <matches.length; i++){
+
+
+
+	let one = new Link(matches[i],matches[i+1])
+	i++
+    links.push(one);
+
+	}
+	console.log(JSON.stringify(links));
+       
+    }; */
+
+
+
+
+
+   
+/*
+const reTxt = /\[(.*?)\]/gi;
+let linkText = markdown.match(reTxt);
+
+const urlRegEx = /(https?|ftp):\/\/([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?/gi;
+let url = markdown.match(urlRegEx);
+
+console.log(url);
+console.log(linkText); */

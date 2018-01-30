26d19c8fc9b3b7e89cc9fc41fbfeecd89692189a
visidevi <32283668+visidevi@users.noreply.github.com>
2018-01-30 18:47:32 -0300

    Revert "Dev"

--- demo/lib/md-link-extractor.js
+++ demo/lib/md-link-extractor.js
@@ -1,11 +1,13 @@
+// Objeto Link:
 class Link {
   constructor(text, href) {
     this.text = text;
     this.href = href;
   }
-};
+}
+
 // Función que extrae links desde el string en formato Markdown:
-markdownLinkExtractor = (markdown) => {
+markdownLinkExtractor = function(markdown) {
   'use strict';
   const re = /!?\[(.*)\]\((.*?)\)/gi;  
 
@@ -32,3 +34,4 @@ markdownLinkExtractor = (markdown) => {
   };
 };
 
+module.exports.markdownLinkExtractor = markdownLinkExtractor;

22c388e0a2953983b46ac0c4ee8cb176f25bf6d3
missmakita <mfernanlav@gmail.com>
2018-02-01 16:05:01 -0300

    merge skyblue

--- demo/js/init.js
+++ demo/js/init.js
@@ -8,7 +8,30 @@ $('#showDemo').click(function() {
   $('#demo').toggle();
 });
 
-var options = [ {selector: '.imgScript', offset: 500, callback: function(el) { Materialize.fadeInImage($(el)); } }, {selector: '.bwr', offset: 500, callback: function(el) { Materialize.fadeInImage($(el)); } }, {selector: '.imgHtml', offset: 500, callback: function(el) { Materialize.fadeInImage($(el)); } } ]; Materialize.scrollFire(options);
+// Scrollfire
+const options = [
+  {
+    selector: '.imgScript',
+    offset: 500,
+    callback: function(el) {
+      Materialize.fadeInImage($(el)); 
+    } 
+  },
+  {
+    selector: '.bwr',
+    offset: 500,
+    callback: function(el) {
+      Materialize.fadeInImage($(el)); 
+    }
+  },
+  {
+    selector: '.imgHtml',
+    offset: 500,
+    callback: function(el) {
+      Materialize.fadeInImage($(el)); 
+    }
+  } 
+]; Materialize.scrollFire(options);
 
 const extract = document.getElementById('extract');
 extract.onclick = function myLinks() {
